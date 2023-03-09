77d97c5a4c38f3080d0560bb5001c00186bf097d
Karsten Gebbert <karsten.gebbert@staffbase.com>
2023-03-09 21:02:00 +0100

    some ui stuff

--- bin/snorkel.rs
+++ bin/snorkel.rs
@@ -1,14 +1,173 @@
-use std::{sync::mpsc, thread};
+use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue, style,
+    terminal::{self, ClearType},
+    Command, Result,
+};
+use snorkel::orca::Orca;
+use std::io::{self, Write};
+use tui::{style::Color, widgets::Paragraph};
 
-fn main() {
-    let (tx, rx) = mpsc::channel::<snorkel::Event>();
-    let t = thread::spawn(|| snorkel::timerfd::run(rx));
+// fn run<W>(w: &mut W) -> Result<()>
+// where
+//     W: Write,
+// {
+//     execute!(w, terminal::EnterAlternateScreen)?;
+//     terminal::enable_raw_mode()?;
 
-    ctrlc::set_handler(move || {
-        tx.send(snorkel::Event)
-            .expect("Could not send signal on channel.")
-    })
-    .expect("Error setting Ctrl-C handler");
+//     let mut orca = Orca::new(20, 20);
 
-    t.join().expect("couldn't join the fun")
+//     loop {
+//         queue!(
+//             w,
+//             style::ResetColor,
+//             terminal::Clear(ClearType::All),
+//             cursor::Hide,
+//             cursor::MoveTo(1, 1)
+//         )?;
+
+//         for line in orca.render().split('\n') {
+//             queue!(w, style::Print(line), cursor::MoveToNextLine(1))?;
+//         }
+
+//         let char = read_char()?;
+//         queue!(
+//             w,
+//             style::Print(format!("char: {}", char)),
+//             cursor::MoveToNextLine(1)
+//         )?;
+
+//         w.flush()?;
+//     }
+
+//     execute!(
+//         w,
+//         style::ResetColor,
+//         cursor::Show,
+//         terminal::LeaveAlternateScreen
+//     )?;
+
+//     terminal::disable_raw_mode()
+// }
+
+// pub fn read_char() -> Result<char> {
+//     loop {
+//         if let Ok(Event::Key(KeyEvent {
+//             code: KeyCode::Char(c),
+//             ..
+//         })) = event::read()
+//         {
+//             return Ok(c);
+//         }
+//     }
+// }
+
+// pub fn buffer_size() -> Result<(u16, u16)> {
+//     terminal::size()
+// }
+
+fn run_app<B: tui::backend::Backend>(terminal: &mut tui::Terminal<B>) -> io::Result<()> {
+    let orca = Orca::new(20, 20);
+    loop {
+        terminal.draw(|f| ui(f, &orca))?;
+        if let Event::Key(key) = event::read()? {
+            if let KeyCode::Char('q') = key.code {
+                return Ok(());
+            }
+            log::info!("char: {:#?}", key);
+        }
+    }
+}
+
+fn ui<B: tui::backend::Backend>(f: &mut tui::Frame<B>, o: &snorkel::orca::Orca) {
+    let chunks = tui::layout::Layout::default()
+        .direction(tui::layout::Direction::Vertical)
+        .constraints(
+            [
+                tui::layout::Constraint::Max(4),
+                tui::layout::Constraint::Percentage(80),
+                tui::layout::Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+
+    let block = tui::widgets::Block::default()
+        .title("Tabs")
+        .borders(tui::widgets::Borders::ALL);
+    f.render_widget(block, chunks[0]);
+
+    let p = Paragraph::new(o.render()).block(
+        tui::widgets::Block::default()
+            .title("Snorkel")
+            .borders(tui::widgets::Borders::ALL),
+    );
+    f.render_widget(p, chunks[1]);
+
+    let tui_w = tui_logger::TuiLoggerWidget::default()
+        .block(
+            tui::widgets::Block::default()
+                .title("Logs")
+                .border_style(
+                    tui::style::Style::default()
+                        .fg(Color::White)
+                        .bg(Color::Black),
+                )
+                .borders(tui::widgets::Borders::ALL),
+        )
+        .output_separator('|')
+        .output_timestamp(Some("%F %H:%M:%S%.3f".to_string()))
+        .output_level(Some(tui_logger::TuiLoggerLevelOutput::Long))
+        .output_target(false)
+        .output_file(false)
+        .output_line(false)
+        .style(
+            tui::style::Style::default()
+                .fg(Color::White)
+                .bg(Color::Black),
+        );
+    f.render_widget(tui_w, chunks[2]);
+}
+
+fn main() -> Result<()> {
+    // ░█▀▀░▀█▀░█▀█░█▀▄░▀█▀░█░█░█▀█
+    // ░▀▀█░░█░░█▀█░█▀▄░░█░░█░█░█▀▀
+    // ░▀▀▀░░▀░░▀░▀░▀░▀░░▀░░▀▀▀░▀░░
+    tui_logger::init_logger(log::LevelFilter::Info).unwrap();
+    tui_logger::set_default_level(log::LevelFilter::Info);
+
+    terminal::enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(
+        stdout,
+        terminal::EnterAlternateScreen,
+        event::EnableMouseCapture
+    )?;
+    let backend = tui::backend::CrosstermBackend::new(stdout);
+    let mut terminal = tui::Terminal::new(backend)?;
+
+    // ░█▀█░█▀█░█▀█
+    // ░█▀█░█▀▀░█▀▀
+    // ░▀░▀░▀░░░▀░░
+
+    let res = run_app(&mut terminal);
+
+    // ░█▀▀░█░█░█░█░▀█▀░█▀▄░█▀█░█░█░█▀█
+    // ░▀▀█░█▀█░█░█░░█░░█░█░█░█░█▄█░█░█
+    // ░▀▀▀░▀░▀░▀▀▀░░▀░░▀▀░░▀▀▀░▀░▀░▀░▀
+
+    terminal::disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        terminal::LeaveAlternateScreen,
+        event::DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
 }
